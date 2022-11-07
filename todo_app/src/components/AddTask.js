73c9ca79b1b57c3f641a776bee55d350798ce7e5
Kuunibe Gracious <graciouskuunibe@gmail.com>
2022-11-07 12:17:03 -0800

    Update AddTask.js

--- todo_app/src/components/AddTask.js
+++ todo_app/src/components/AddTask.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import "../css/AddTask.css"
 
 const AddTask = (props) => {
   return (
     <div className="add_task">
-          <input type="text" onChange={props.handleValueChange} />
+          <input type="text" onChange={props.handleValueChange} placeholder="Input Task"/>
           <button onClick={props.addTask}>Add Task</button>
     </div>
   )
