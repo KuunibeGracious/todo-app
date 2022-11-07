c9a9d7fb4f0259f4edde307386704c788d1c81b9
Kuunibe Gracious <graciouskuunibe@gmail.com>
2022-11-07 12:17:44 -0800

    Update App.js

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -1,6 +1,10 @@
 import { useState } from "react";
+import Header from "./components/Header";
 import AddTask from "./components/AddTask";
 import TodoList from "./components/TodoList";
+import DeleteAll from "./components/DeleteAll";
+import RemoveTask from "./components/RemoveTask";
+import "./css/App.css"
 
 
 function App() {
@@ -15,32 +19,55 @@ function App() {
   const addTask = () => {
     const task = {
       id:todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1 ,
-      taskName: newTask
+      taskName: newTask,
+      checked:false
     }
     setTodoList([...todoList, task]);
   }
-
+  
   //function to remove Task from todo list
   const removeTask = (id) => {
     setTodoList(todoList.filter((task) => {
       return task.id !== id
     }))
   }
+  //function to delete all tasks
+  const deleteAll = () => {
+    setTodoList([])
+  }
+  //check tasks
+  const handleCheck = (id) => {
+    setTodoList(todoList.map((task) => task.id === id ? { ...task, checked: !task.checked } : task))
+  }
+
+  const handleInputField = () => {
+    setNewTask("")
+  }
+
 
   return (
-    <div className="App">
+    <div className="app">
+      <Header/>
       <AddTask
         handleValueChange={handleValueChange}
         addTask={addTask}
+        clearField={handleInputField}
       />
       <TodoList todoList={todoList.map((task) => {
         return (
-          <div key={task.id}>
-            <p>{task.taskName}</p>
-            <button onClick={() => removeTask(task.id)}>remove</button>
+          <div className="todo_list" key={task.id}>
+            <input
+              className="checkbox"
+              type="checkbox"
+              onChange={() => handleCheck(task.id)}
+              checked={task.checked}
+            />
+            <p className="task">{task.taskName}</p>
+            <RemoveTask className="remove_task" removeTask={() => removeTask(task.id)} />
           </div>
         ) 
       })} />
+      {todoList.length > 0 ?  <DeleteAll deleteAll={deleteAll} /> : <p className="footer_note">add a task</p>}
     </div>
   );
 }
