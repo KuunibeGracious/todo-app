bbc414d5780caef6bf18a082752ead6e1e72b052
Kuunibe Gracious <graciouskuunibe@gmail.com>
2022-11-07 05:40:22 -0800

    Create AddTask.js

---
+++ todo_app/src/components/AddTask.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AddTask = () => {
+  return (
+    <div className="add_task">
+          <input type="text" />
+          <button>Add Task</button>
+    </div>
+  )
+}
+
+export default AddTask
